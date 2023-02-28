9b7d07943b1e0dafaec1b1ed2f69e868bc79821c
unknown <miguellodige@gmail.com>
2023-02-27 22:17:15 -0300

    salvar2

---
+++ maria.py
@@ -0,0 +1,18 @@
+#o seu comentario 
+print("MIGUEL, MACHADO")
+ 
+print(20"ANOS")
+
+print( 85"KG"sep=":):)" )
+print(1.85"ALTURA", 20, sep="+#+#+#")
+
+print (30 == 30)
+print(30 == 31)
+
+print(type (10 == 31))
+
+print(type("teste"))
+
+print(type(50))
+
+print(11,1.1, True)
